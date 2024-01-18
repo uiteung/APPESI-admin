6e74ae35725b96d91138ab09b7ccd88ade7878f2
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-19 01:28:38 +0700

    perbaikan load content untuk sidebar & navbar

--- static/theme/bar/navbar.js
+++ static/theme/bar/navbar.js
@@ -1,5 +1,5 @@
 // Untuk Render Footer
 document.addEventListener('DOMContentLoaded', function() {
-    $(".app-header").load("https://euis.ulbi.ac.id/APPESI/static/navbar.html");
+    $(".app-header").load("https://euis.ulbi.ac.id/APPESI-admin/static/navbar.html");
     console.log('DOM fully loaded and parsed');
   });
--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,5 +1,5 @@
 // Untuk Render Sidebar
 document.addEventListener('DOMContentLoaded', function() {
-  $(".sidebar").load("https://euis.ulbi.ac.id/APPESI/static/sidebar.html");
+  $(".sidebar").load("https://euis.ulbi.ac.id/APPESI-admin/static/sidebar.html");
   console.log('DOM fully loaded and parsed');
 });
