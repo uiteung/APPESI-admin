42f2af40bcf4c49b3542db61c53e5d7f679022ae
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-23 09:45:07 +0700

    hide column di mahasiswa uji

--- static/dosen/mahasiswa_uji_p3.js
+++ static/dosen/mahasiswa_uji_p3.js
@@ -79,13 +79,6 @@ CihuyDomReady(() => {
                         <td>
                             <p class="fw-bold mb-1">${waktuSidangFormatted}</p>
                         </td>
-                        <td>
-                            <p class="fw-bold mb-1">Belum Ada</p>
-                            <p class="fw-bold mb-1">Belum Ada</p>
-                        </td>
-                        <td>
-                        <a href="input_nilai_bimbingan_p3.html" type="button" class="btn btn-info m-1" style="font-size:10px">Nilai</a>
-                        </td>
                     </tr>`;
                 }
             });
