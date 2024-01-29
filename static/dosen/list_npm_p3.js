7066146764c91e287ea498432fe976c135650b7a
HeroBuxx <me@herobuxx.me>
2024-01-29 15:59:23 +0700

    Cleanup code
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- static/dosen/list_npm_p3.js
+++ static/dosen/list_npm_p3.js
@@ -21,21 +21,6 @@ CihuyDomReady(() => {
         }
     };
 
-    // Mapping NIDN ke Nama
-    const codeToNameMapping = {
-        "0420058801" : "Roni Andarsyah, S.T.,M.Kom.,SFPC",
-        "0427078401" : "Cahyo Prianto, S.Pd.,M.T.,CDSP.,SFPC",
-        "0407117405" : "M. Yusril Helmi Setyawan, S.Kom.,M.Kom.,SFPC",
-        "0410118609" : "Rolly Maulana Awangga, S.T.,MT.,CAIP,SFPC",
-        "0402058005" : "Mohamad Nurkamal Fauzan, S.T.,M.T.,SFPC",
-        "0423127804" : "Roni Habibi, S.Kom.,M.T.,SFPC",
-        "0416048803" : "Syafrial Fachri Pane,ST. M.TI.,EBDP.,CDSP.,SFPC",
-        "0402047205" : "Rd. Nuraini Siti Fatonah, S.S.,M.Hum.,SFPC",
-        "0415048901" : "Nisa Hanum Harani, S.Kom.,M.T.,CDSP.,SFPC",
-        "0415107901" : "Woro Isti Rahayu, S.T.,M.T.,SFPC",
-        "0403117607" : "Noviana Riza, S.Si.,M.T.,SFPC",
-    };
-
     // Untuk Get All Data Pendaftar
     fetch(UrlGetAllPersyaratan, requestOptions)
     .then((result) => {
@@ -86,37 +71,7 @@ CihuyDomReady(() => {
 		const akhirindex = mulaiindex + itemPerPage;
 
 		for (let i = 0; i < baris.length; i++) {
-			if (i >= mulaiindex && i < akhirindex) {
-				baris[i].style.display = "table-row";
-			} else {
-				baris[i].style.display = "none";
-			}
+			baris[i].style.display = "none";
 		}
 	}
-
-    // Fungsi untuk Update Pagination
-    function updatePagination() {
-        halamanSaatIni.textContent = `Halaman ${halamannow}`;
-    }
-
-    // Button Pagination (Sebelumnya)
-    buttonPreviousPage.addEventListener("click", () => {
-        if (halamannow > 1) {
-            halamannow--;
-            displayData(halamannow);
-            updatePagination();
-        }
-    });
-
-    // Button Pagination (Selanjutnya)
-	buttonNextPage.addEventListener("click", () => {
-		const totalPages = Math.ceil(
-			tablebody.querySelectorAll("#tablebody tr").length / itemPerPage
-		);
-		if (halamannow < totalPages) {
-			halamannow++;
-			displayData(halamannow);
-			updatePagination();
-		}
-	});
 });
