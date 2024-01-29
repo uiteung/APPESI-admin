ee2a420c4e9ea5b570e3faf7802eec7373d7a24d
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-29 15:10:43 +0700

    add endpoint UrlPostRevisiP3 untuk post revisi p3

--- static/controller/template.js
+++ static/controller/template.js
@@ -6,3 +6,4 @@ export let UrlGetAllPersyaratan = "https://p3-be.ulbi.ac.id/api/sidang/syarat/al
 // Endpoint POST
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
 export let UrlPostRevisi = "https://kimteungbim.ulbi.ac.id/revisi/";
+export let UrlPostRevisiP3 = "https://kimteungbim.ulbi.ac.id/sidang/p3/revisi/";
