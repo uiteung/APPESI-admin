c1eba9c38b123e7707fb1dc07a35e95b87e4b087
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-31 11:47:46 +0700

    coba create untuk change form

--- static/style/style_input_nilai_p3.js
+++ static/style/style_input_nilai_p3.js
@@ -65,22 +65,73 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Untuk Ubah Style Form
-// Menangkap elemen select
-var selectPosisiAnggota2 = document.getElementById('selectPosisiAnggota2');
+document.addEventListener('DOMContentLoaded', function() {
+  // Untuk Ubah Style Form
+  // Menangkap elemen select
+  var selectPosisiAnggota2 = document.getElementById('selectPosisiAnggota2');
+  var titleForm = document.getElementById('titleForm');
+  var formInput = document.getElementById('formInputBE')
+  var labelNilai1 = document.getElementById('labelNilai1');
+  var labelNilai2 = document.getElementById('labelNilai2');
+  var labelNilai3 = document.getElementById('labelNilai3');
+  var labelNilai4 = document.getElementById('labelNilai4');
 
-// Menangkap elemen judul form
-var titleForm = document.getElementById('titleForm');
+  // Menambahkan event listener untuk mendeteksi perubahan pada elemen select
+  selectPosisiAnggota2.addEventListener('change', function() {
+    // Memperbarui judul form berdasarkan pilihan yang dipilih
+    if (selectPosisiAnggota2.value === 'Frontend') {
+      titleForm.innerText = 'Penilaian Frontend Developer';
+      formInput.id = 'formInputFE';
+      labelNilai1.innerText = 'Semua JS Dirilis Versi di JSDelivr dari Semua JS Buatan Sendiri yang Dipakai Frontend';
+      labelNilai2.innerText = 'Pemanggilan JSDelivr Pada HTML yang Sudah Pasti Pakai Type Module';
+      labelNilai3.innerText = 'Kelengkapan CSS, Favicon, 404.html, Modal, Transisi, Loading, Responsive dan Hasil Analisis dari gtmetrix.com';
+      labelNilai4.innerText = 'Memakai Custom Domain pada GitHub Pages nya';
 
-// Menambahkan event listener untuk mendeteksi perubahan pada elemen select
-selectPosisiAnggota2.addEventListener('change', function() {
-  // Memperbarui judul form berdasarkan pilihan yang dipilih
-  if (selectPosisiAnggota2.value === 'Frontend') {
-    titleForm.innerText = 'Penilaian Frontend Developer';
-  } else if (selectPosisiAnggota2.value === 'Backend') {
-    titleForm.innerText = 'Penilaian Backend Developer';
-  } else {
-    // Default jika tidak ada pilihan yang dipilih
-    titleForm.innerText = 'Penilaian Developer';
-  }
+      // Mengganti ID dan placeholder untuk input nilai sesuai dengan Frontend
+      document.getElementById('inputNilaiBE1').id = 'inputNilaiFE1';
+      document.getElementById('inputNilaiFE1').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiBE2').id = 'inputNilaiFE2';
+      document.getElementById('inputNilaiFE2').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiBE3').id = 'inputNilaiFE3';
+      document.getElementById('inputNilaiFE3').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiBE4').id = 'inputNilaiFE4';
+      document.getElementById('inputNilaiFE4').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+
+    } else if (selectPosisiAnggota2.value === 'Backend') {
+      titleForm.innerText = 'Penilaian Backend Developer';
+      formInput.id = 'formInputBE';
+      labelNilai1.innerText = 'Semua Endpoint Berfungsi Termasuk Otorisasi dan Autentikasi Token';
+      labelNilai2.innerText = 'Package Dibuat Sendiri dan Semua Digunakan';
+      labelNilai3.innerText = 'Semua Endpoint di GCF Memakai Package yang Dibuat dan Lokasi Server di Jakarta';
+      labelNilai4.innerText = 'Terintegrasi dengan wa.my.id';
+
+      // Mengganti ID dan placeholder untuk input nilai sesuai dengan Backend
+      document.getElementById('inputNilaiFE1').id = 'inputNilaiBE1';
+      document.getElementById('inputNilaiBE1').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE2').id = 'inputNilaiBE2';
+      document.getElementById('inputNilaiBE2').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE3').id = 'inputNilaiBE3';
+      document.getElementById('inputNilaiBE3').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE4').id = 'inputNilaiBE4';
+      document.getElementById('inputNilaiFE4').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+    } else {
+      // Default jika tidak ada pilihan yang dipilih
+      titleForm.innerText = 'Penilaian Developer';
+      formInput.id = 'formInputBE';
+      labelNilai1.innerText = 'Semua Endpoint Berfungsi Termasuk Otorisasi dan Autentikasi Token';
+      labelNilai2.innerText = 'Package Dibuat Sendiri dan Semua Digunakan';
+      labelNilai3.innerText = 'Semua Endpoint di GCF Memakai Package yang Dibuat dan Lokasi Server di Jakarta';
+      labelNilai4.innerText = 'Terintegrasi dengan wa.my.id';
+
+      // Mengganti ID dan placeholder untuk input nilai sesuai dengan default
+      document.getElementById('inputNilaiFE1').id = 'inputNilaiBE1';
+      document.getElementById('inputNilaiBE1').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE2').id = 'inputNilaiBE2';
+      document.getElementById('inputNilaiBE2').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE3').id = 'inputNilaiBE3';
+      document.getElementById('inputNilaiBE3').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+      document.getElementById('inputNilaiFE4').id = 'inputNilaiBE4';
+      document.getElementById('inputNilaiBE4').placeholder = 'Inputkan Nilai dari Rentang 0 sampai 100';
+    }
+  });
 });
