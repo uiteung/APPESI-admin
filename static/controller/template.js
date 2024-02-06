dccd8595807bec2ff194b16ba8318fff46b1524e
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-06 19:46:31 +0700

    add UrlGetAllNilaiP3

--- static/controller/template.js
+++ static/controller/template.js
@@ -3,6 +3,7 @@ export let UrlCekPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syar
 export let UrlCekRole = "https://p3-be.ulbi.ac.id/api/sidang/dosen";
 export let UrlGetAllPersyaratan = "https://p3-be.ulbi.ac.id/api/sidang/syarat/all";
 export let UrlGetPersyaratanByNIDN = "https://p3-be.ulbi.ac.id/api/sidang/syarat/nidn";
+export let UrlGetAllNilaiP3 = "https://kimteungbim.ulbi.ac.id/sidang/p3/nilai";
 
 // Endpoint POST
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
